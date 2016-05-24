17dcea910749f7b32ef6e3022043e68f67f40ad8
Alex White <alex.white@codiac.io>
2016-05-24 22:14:45 +0000

    added typings support with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,15 @@ var concat = require('gulp-concat');
 var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 var merge = require('merge2');
+var gulpTypings = require('gulp-typings');
 
+
+//Typings install
+gulp.task('typings', function() {
+	var stream = gulp.src("./typings.json")
+		.pipe(gulpTypings()); //will install all typingsfiles in pipeline. 
+	return stream; // by returning stream gulp can listen to events from the stream and knows when it is finished
+})
 
 //Node JS TypeScript Compile
 gulp.task('node-ts', function() {
@@ -12,16 +20,18 @@ gulp.task('node-ts', function() {
 	var tsProject = ts.createProject('server/tsconfig.json');
 	
 	var tsResult = gulp.src('server/src/**/*.ts')
-	.pipe(sourcemaps.init()) //
+	.pipe(sourcemaps.init()) 
 	.pipe(ts(tsProject))
 	
 	return merge([
 		tsResult.dts.pipe(gulp.dest('server/build/definitions')),
-		tsResult.js.pipe(gulp.dest('server/build/js'))
+		tsResult.js.pipe(gulp.dest('server/build/'))
 	]);
 	
 	
 });
+
+
 
 gulp.task('default', ['node-ts']);
 
